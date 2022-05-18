3b2d1ce797732615fe060e64f0ef93c528a227e2
Jhonattan Tunales <70348160+RostCode78@users.noreply.github.com>
2022-05-18 18:55:43 -0500

    Feat: Iniciando archivo Layout

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,11 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import { PrismaClient } from '@prisma/client';
+import Layout from '../layout/Layout';
 
-export default function Home({ categorias }) {
-
-  console.log(categorias);
-
-  return (
-    <h1>Hola amigo</h1>
-  )
-}
-
-export const getServerSideProps = async () => {
-
-  const prisma = new PrismaClient();
-
-  // De esta formas definimos un where.
-  // Es decir que nos trae el parametro con el valor escrito.
-  // const categorias = await prisma.categoria.findFirst({
-  //   where: {
-  //     nombre: "Pizzas"
-  //   }
-  // })
-
-  const categorias = await prisma.categoria.findMany();
-
-  return {
-    props: {
-      categorias
-    }
-  }
-
+export default function Home() {
+    return (
+      <Layout>
+        <h1>Hola amigo</h1>
+      </Layout>
+    )
 }
